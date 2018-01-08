e787ddeaf53fbb2ee1a22a2f63810a1478765c3e
Archana Dixit <archanavdixit@yahoo.com>
2018-01-08 14:17:42 -0600

    Add project files.

---
+++ DictionaryPhoneBook/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+            int n = int.Parse(Console.ReadLine());
+            string[] userInput = new string[2];
+            //For loop for Adding entries in the phonebook//
+            for (int i = 0; i < n; i++)
+            {
+                userInput = Console.ReadLine().Split(' ');
+                phoneBook.Add(userInput[0], userInput[1]);
+            }
+
+            string name = Console.ReadLine();
+            while (!string.IsNullOrEmpty(name))
+            {
+                if (phoneBook.ContainsKey(name))
+                {
+                    Console.WriteLine($"{name}={phoneBook[name]}");
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+                name = Console.ReadLine();
+            } 
+            Console.ReadLine();
+        }
+    }
+}
